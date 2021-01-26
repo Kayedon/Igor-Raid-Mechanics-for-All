88f4e4913fde070ed2011abc94c0c5e6dc90d066
Taranchuk <nfhfyxer@gmail.com>
2021-01-26 06:08:08 +0200

    update

---
+++ Source/IgorRaidMechanics/IgorRaidMechanicsMod.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using RimWorld;
+using UnityEngine;
+using VanillaStorytellersExpanded;
+using Verse;
+
+namespace IgorRaidMechanics
+{
+    public class IgorRaidMechanicsMod : Mod
+    {
+        public static IgorRaidMechanicsSettings settings;
+        public IgorRaidMechanicsMod(ModContentPack pack) : base(pack)
+        {
+            settings = GetSettings<IgorRaidMechanicsSettings>();
+        }
+        public override void DoSettingsWindowContents(Rect inRect)
+        {
+            base.DoSettingsWindowContents(inRect);
+            settings.DoSettingsWindowContents(inRect);
+        }
+
+        public override string SettingsCategory()
+        {
+            return "[KYD] Igor Raid Mechanics for All";
+        }
+
+        public override void WriteSettings()
+        {
+            base.WriteSettings();
+            DefsAlterer.DoDefsAlter();
+        }
+    }
+    [StaticConstructorOnStartup]
+    public static class DefsAlterer
+    {
+        static DefsAlterer()
+        {
+            DoDefsAlter();
+        }
+        public static void DoDefsAlter()
+        {
+            if (IgorRaidMechanicsMod.settings.firstTimeInit)
+            {
+                IgorRaidMechanicsMod.settings.damageMultiplier = 2f;
+                IgorRaidMechanicsMod.settings.disableThreatsAtPopulationCount = 1;
+                IgorRaidMechanicsMod.settings.goodIncidents = new List<string>
+                {
+                    "ResourcePodCrash",
+                    "PsychicSoothe",
+                    "SelfTame",
+                    "AmbrosiaSprout",
+                    "FarmAnimalsWanderIn",
+                    "WandererJoin",
+                    "RefugeePodCrash",
+                    "ThrumboPasses",
+                    "MeteoriteImpact",
+                    "WildManWandersIn"
+                };
+                IgorRaidMechanicsMod.settings.firstTimeInit = false;
+            }
+
+            foreach (var storytellerDef in DefDatabase<StorytellerDef>.AllDefs)
+            {
+                var modExtension = storytellerDef.GetModExtension<StorytellerDefExtension>();
+                if (modExtension is null)
+                {
+                    modExtension = new StorytellerDefExtension();
+                    if (storytellerDef.modExtensions is null) 
+                        storytellerDef.modExtensions = new List<DefModExtension>();
+                    storytellerDef.modExtensions.Add(modExtension);
+                }
+                AssignValues(ref modExtension);
+            }
+        }
+
+        private static void AssignValues(ref StorytellerDefExtension modExtension)
+        {
+            modExtension.storytellerThreat = new StorytellerThreat();
+            modExtension.storytellerThreat.allDamagesMultiplier = IgorRaidMechanicsMod.settings.damageMultiplier;
+            modExtension.storytellerThreat.disableThreatsAtPopulationCount = IgorRaidMechanicsMod.settings.disableThreatsAtPopulationCount;
+            modExtension.storytellerThreat.goodIncidents = IgorRaidMechanicsMod.settings.goodIncidents;
+        }
+    }
+}
---
+++ Source/IgorRaidMechanics/IgorRaidMechanicsSettings.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using RimWorld;
+using UnityEngine;
+using Verse;
+using Verse.AI;
+
+namespace IgorRaidMechanics
+{
+    public class IgorRaidMechanicsSettings : ModSettings
+    {
+        public float damageMultiplier;
+        public int disableThreatsAtPopulationCount;
+        public List<string> goodIncidents = new List<string>();
+        public bool firstTimeInit = true;
+        public override void ExposeData()
+        {
+            base.ExposeData();
+            Scribe_Values.Look(ref firstTimeInit, "firstTimeInit");
+            Scribe_Values.Look(ref damageMultiplier, "damageMultiplier");
+            Scribe_Values.Look(ref disableThreatsAtPopulationCount, "disableThreatsAtPopulationCount");
+            Scribe_Collections.Look(ref goodIncidents, "goodIncidents");
+        }
+
+        public void DoSettingsWindowContents(Rect inRect)
+        {
+            var goodIncidentsLocal = goodIncidents.Where(x => DefDatabase<IncidentDef>.GetNamed(x) != null).OrderBy(x => DefDatabase<IncidentDef>.GetNamed(x).label).ToList();
+            var allIncidents = DefDatabase<IncidentDef>.AllDefs.Where(x => !goodIncidents.Contains(x.defName)).OrderBy(x => x.label).ToList();
+            Rect rect = new Rect(inRect.x, inRect.y, inRect.width, inRect.height);
+            Rect rect2 = new Rect(0f, 0f, inRect.width - 30f, 200 + (goodIncidentsLocal.Count * 24) + (allIncidents.Count * 24));
+            Widgets.BeginScrollView(rect, ref scrollPosition, rect2, true);
+            Listing_Standard listingStandard = new Listing_Standard();
+            listingStandard.Begin(rect2);
+            if (listingStandard.ButtonText("Reset".Translate()))
+            {
+                firstTimeInit = true;
+                DefsAlterer.DoDefsAlter();
+            }
+            listingStandard.SliderLabeled("Igor.AdjustDamageMultiplier".Translate(), ref damageMultiplier, damageMultiplier.ToStringDecimalIfSmall(), 0.1f, 5f);
+            listingStandard.SliderLabeled("Igor.AdjustMinPopulationForThreats".Translate(), ref disableThreatsAtPopulationCount, disableThreatsAtPopulationCount.ToString(), 0, 99);
+            listingStandard.Label("Igor.PickGoodIncidentToFire".Translate());
+            listingStandard.GapLine();
+            foreach (var incident in goodIncidentsLocal)
+            {
+                bool test = true;
+                listingStandard.CheckboxLabeled(DefDatabase<IncidentDef>.GetNamed(incident).label, ref test);
+                if (!test)
+                {
+                    goodIncidents.Remove(incident);
+                }
+            }
+            listingStandard.Gap();
+            foreach (var incident in allIncidents)
+            {
+                bool test = false;
+                listingStandard.CheckboxLabeled(incident.label, ref test);
+                if (test)
+                {
+                    goodIncidents.Add(incident.defName);
+                }
+            }
+
+
+            listingStandard.End();
+            Widgets.EndScrollView();
+            base.Write();
+        }
+        private static Vector2 scrollPosition = Vector2.zero;
+    }
+}
